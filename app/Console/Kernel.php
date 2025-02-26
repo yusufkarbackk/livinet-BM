6bf40929cd6e6cc62c1f83f94cc15662393fc425
yusufkarbackk <yusufkarback64@gmail.com>
2025-02-26 14:19:58 +0700

    test to run job every minute

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,7 +13,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-        $schedule->job(new GetWhmcsData())->everyFiveMinutes();
+        $schedule->job(new GetWhmcsData())->everyMinute();
     }
 
     /**
